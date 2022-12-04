c29a7048f0c13d778149aee7925a1129b6faac17
Tamara <Tamara-1981_0@mail.ru>
2022-12-05 00:46:38 +0700

    задача 9

---
+++ task9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка (10, 99)
+// и показывает наибольшую цифру числа например: 78 -8, 12 -2, 85 -8.
+
+int numberN = new Random().Next(10, 99); 
+Console.WriteLine(numberN);
+
+int a = numberN/10;
+int b = numberN%10;
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+
+Console.Write("max ");
+Console.WriteLine(max);
+
+
