493d55c6e35317b13aee988a643ca71bdfd2fa9a
Tamara <Tamara-1981_0@mail.ru>
2022-12-04 23:10:06 +0700

    задача 0

---
+++ task0/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число 
+//и выдает его квадрат (число умноженное само на себя)
+
+int a;
+
+int b;
+
+Console.Write("Enter number a = ");
+
+a = Convert.ToInt32(Console.ReadLine ());
+
+b = a*a;
+
+Console.Write("Square of number a is b = ");
+
+Console.Write(b);
