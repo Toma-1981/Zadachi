247e424f0487e82a0a095a93e8dcd7b43bc2a41a
Tamara <Tamara-1981_0@mail.ru>
2022-12-05 01:01:56 +0700

    задача 10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int numberN = new Random().Next(100, 999); 
+Console.WriteLine(numberN);
+
+int a = numberN/10;
+int b = a%10;
+
+Console.Write("Вторая цифра ");
+Console.WriteLine(b);
