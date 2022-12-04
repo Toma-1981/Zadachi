2d5803bd1e5c455f4e1cf114f01e33f81fe12f58
Tamara <Tamara-1981_0@mail.ru>
2022-12-05 04:02:55 +0700

    задача 10, решала не сама, сложная

---
+++ task13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает что третьей цифры нет.
+
+Console.Write("Введите число a = ");
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k < 0 ? "нет третьей цифры " : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine();
