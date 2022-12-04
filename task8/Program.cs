3d60e1ccc062f2b5a68e8cb3a2591f34d855ccef
Tamara <Tamara-1981_0@mail.ru>
2022-12-04 23:39:37 +0700

    задача 8

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая, на вход принимает число N и 
+// на выходе показывает все четные числа от 1 до N.
+
+ Console.Write("Введите число ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 2;
+       
+while (res <= num)
+    {
+        Console.WriteLine(res);
+        res += 2;
+    }
