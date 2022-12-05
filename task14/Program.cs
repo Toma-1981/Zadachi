7f991f67133c1c190e946f4e705d52ba8ac2d7f2
Tamara <Tamara-1981_0@mail.ru>
2022-12-05 14:43:56 +0700

    задача 14

---
+++ task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход число
+// и проверяет кратно ли оно одновременно 7 и 23
+
+
+Console.Write("Введите число ");
+int N = int.Parse(Console.ReadLine()!);
+
+if (N%7 == 0 && N%23 == 0)
+{
+    Console.WriteLine ("-----> да");
+}
+
+else 
+{
+    Console.WriteLine ("----> нет ");
+}
