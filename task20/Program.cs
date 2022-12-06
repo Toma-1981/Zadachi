e2642fa7d66c1c4ed1e6f9c2502db6b7047f0743
Tamara <Tamara-1981_0@mail.ru>
2022-12-06 19:56:00 +0700

    задача 20

---
+++ task20/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу которая принимает на вход координаты
+//двух точек и находит расстояние между ними в 2D пространстве.
+
+// А (3,6) В (2,1) --> 5,09
+// A (7,-5) B (1,-1) --> 7,21
+
+Console.Clear();
+Console.Write("Введите координату хА ");
+int xA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату yА ");
+int yA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату хB ");
+int xB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату yB ");
+int yB = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt((xA-xB)*(xA-xB)+(yA-yB)*(yA-yB));
+Console.WriteLine(result);
