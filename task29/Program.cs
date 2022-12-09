6cece1b123b4dda2959fc075f395eba50800c10f
Tamara <Tamara-1981_0@mail.ru>
2022-12-09 20:35:25 +0700

    задача 29

---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+//массив из 8 элементов и выводит их на экран.
+Console.Clear();
+int n = 8;
+int[] arr = new int[n];
+Random rand = new Random();
+int[] GetNewArr(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 100);
+        Console.Write(arr[i] +" ");
+
+    }
+    return arr;
+}
+GetNewArr(arr);
