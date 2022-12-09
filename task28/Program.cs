eafb5f8079d20e95eadc1e667310235226784827
Tamara <Tamara-1981_0@mail.ru>
2022-12-09 20:13:25 +0700

    задача 28

---
+++ task28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N. 
+// например: 4 -> 24  
+//           5 -> 120
+Console.Clear();
+Console.Write("Введите число: "); 
+int a = int.Parse(Console.ReadLine()); 
+Console.WriteLine($"Произведение чисел = {ProductOfNumbers(a)}"); 
+int ProductOfNumbers(int number) 
+{     
+    int sum = 1;     
+    while (number > 0)     
+        {   
+            //Console.Write($"{number} ");         
+            sum *= number;         
+            number--;     
+        }     
+    return sum; 
+}
