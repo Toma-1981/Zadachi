ad9537ebb461d223424d52fc6c69225a3dcf1431
Tamara <Tamara-1981_0@mail.ru>
2022-12-20 16:28:22 +0700

    задача 44(1)

--- task44/Program.cs
+++ task44/Program.cs
@@ -4,7 +4,7 @@
 //Если N = 3 -> 0 1 1
 //Если N = 7 -> 0 1 1 2 3 5 8
 
-int num = int.Parse(Console.ReadLine()!);
+/*int num = int.Parse(Console.ReadLine()!);
 int[] listFib = new int[num];
 Console.Write(listFib[0]);
 listFib[1] = 1;Console.Write(listFib[1]);
@@ -12,4 +12,14 @@ for (int i = 2; i < num; i++)
 {
     listFib[i] = listFib[i-1]+listFib[i-2];
     Console.Write(listFib[i]);
-}
+}*/
+
+ int Fibonacci(int n)
+ {
+     if (n == 1 || n == 2) return 1;
+     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+ }
+ for (int i = 1; i < 10; i++)
+ {
+     Console.Write(Fibonacci(i));
+ }
