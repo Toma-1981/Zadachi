114d55991d284285a54cc3f1080010712d50885b
Tamara <Tamara-1981_0@mail.ru>
2022-12-21 14:58:54 +0700

    задача 41

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,9 +1,12 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, с
+//колько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите числа через пробел:  ");
-int[] array = Console.ReadLine()!.Split(' ').Select(x => int.Parse(x)).ToArray();
+
+/*Console.Write("Введите числа через пробел: ");
+
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
 
 int Numbers(int[] array)
 {
@@ -15,4 +18,18 @@ int Numbers(int[] array)
     }
     return count;
 }
-Console.WriteLine(Numbers(array));
+Console.WriteLine(Numbers(array));*/
+
+void CountNumbers(string[] listNum)
+{
+    int count = 0;
+    foreach (string el in listNum)
+    {
+        if (int.Parse(el)>0) count++;
+        Console.Write($"{el} ");
+    }
+    Console.Write($"Количество чисел > 0 = {count}");
+}
+Console.WriteLine("Введите чисела через пробел: ");
+string[] list = (Console.ReadLine()!).Split(' ');
+CountNumbers(list);
